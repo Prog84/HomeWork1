1e344758c15fa75344b51402ade2d90504d1e2da
Prog84 <bvv198424@rambler.ru>
2021-01-29 10:29:54 +1000

    Initial commit

---
+++ Assets/Scripts/DestroyBalls.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DestroyBalls : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Ball ball))
+        {
+            Destroy(ball.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _template;
+    [SerializeField] private Transform[] _spawnPoints;
+
+    private void Start()
+    {
+        SpawnBalls(_template, _spawnPoints);
+    }
+
+    private void SpawnBalls(GameObject template, Transform[] spawnPoints)
+    {
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            Instantiate(template, spawnPoints[i].position, Quaternion.identity);
+        }
+    }
+}
