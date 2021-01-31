f97e0087e6a68485f1a5f9107b1aba2067d74fbf
Prog84 <bvv198424@rambler.ru>
2021-01-31 10:24:46 +1000

    Delete ball script, rename and update DestroyBalls to DestroyObjects

---
+++ Assets/Scripts/DestroyObjects.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class DestroyObjects : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
